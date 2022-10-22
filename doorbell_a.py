2c98cde74f6e7ce4329dde1eec3a2e00ac6903da
rhubarbdog <rhubarbdog76@gmail.com>
2022-10-22 22:49:01 +0100

    A door bell

---
+++ doorbell_a.py
@@ -0,0 +1,16 @@
+from microbit import *
+import music
+import radio
+
+radio.config(channel = 1)
+radio.on()
+
+
+while True:
+    if button_a.is_pressed():
+        radio.send('["G4:4", "C4:4"]')
+        display.show(Image.SQUARE)
+        sleep(1000)
+        display.show(' ')
+        
+    sleep(100)
---
+++ doorbell_b.py
@@ -0,0 +1,16 @@
+from microbit import *
+import music
+import radio
+
+radio.config(channel = 1)
+radio.on()
+
+
+while True:
+    play = radio.receive()
+    if play != None:
+        display.show(Image.SQUARE)
+        music.play(eval(play))
+        display.show(' ')
+        
+    sleep(100)
